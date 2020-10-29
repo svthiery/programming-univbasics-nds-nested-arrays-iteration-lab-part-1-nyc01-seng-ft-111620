2e2354f672c85bd2f175a9096b9a052485a5ec22
Sarah Kain Thiery <sarahvkain@gmail.com>
2020-10-29 15:17:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,12 +1,3 @@
 def find_even_values(src)
-  row_index = 0 
-  while row_index < src.count do 
-    element_index = 0 
-    while element_index < src[row_index].count do
-      if src[row_index][element_index] % 2 == 0 
-        p src[row_index][element_index]
-      end
-      element_index += 1 
-    end
-  row_index += 1 
+
 end
